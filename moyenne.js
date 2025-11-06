060b433e8adab9fa99426132ad7312257b58d8c2
maria8a8 <laim0280@gmail.com>
2025-11-06 15:45:12 +0100

    first commit

---
+++ moyenne.js
@@ -0,0 +1,18 @@
+let x
+let y
+let z
+let a 
+let m
+let moyenne
+x=Number( prompt("entrez la note x "))
+console.log(x)
+y=Number( prompt("etrer la note y "))
+console.log(y)
+z=Number( prompt("etrer la note z "))
+console.log(z)
+a=Number( prompt("etrer la note a "))
+console.log(a)
+m=Number( prompt("etrer la note m "))
+console.log(x)
+moyenne=(x+y+z+a+m)/5
+console.log("la moyenne est : ",moyenne)
